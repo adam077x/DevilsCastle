fcf0a18cac2e65b076de6b366537d0194cd65542
adam.heczko@gmail.com <adam.heczko@gmail.com>
2020-05-20 20:48:08 +0200

    Enemy death, animation, new sprite

--- Assets/Scripts/Entity/HostileEntity.cs
+++ Assets/Scripts/Entity/HostileEntity.cs
@@ -6,13 +6,15 @@ using UnityEngine.AI;
 public class HostileEntity : Entity
 {
     public int health;
-    public Transform eye;
-    private Rigidbody rb;
-    public NavMeshAgent navMeshAgent;
     public float sightDistance;
     public float damageDistance;
     public int damage;
-    private bool attacking;    
+
+    private Rigidbody rb;
+    private NavMeshAgent navMeshAgent;
+    private bool attacking;
+    private Animator animator;
+
     public HostileEntity(int health, float sightDistance, int damage, float damageDistance) 
     {
         this.health = health;
@@ -21,7 +23,6 @@ public class HostileEntity : Entity
         this.damage = damage;
         attacking = false;
     }  
-
     public void Hit(int damage) 
     {
         health -= damage;
@@ -30,33 +31,50 @@ public class HostileEntity : Entity
     public override void Start() 
     {
         rb = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
+
+    bool onground = false;
 
     public override void Update() 
     {
         base.Update();
+
+        if(onground) 
+        {
+        }
+
+        if(health <= 0) 
+        {
+            animator.SetTrigger("Die");
+            onground = true;
+            navMeshAgent.Stop();
+            return;
+        }
 
         if(PlayerHealth.dead) return;
 
         if(Vector3.Distance(transform.position, target.position) < sightDistance) 
         {
             navMeshAgent.SetDestination(target.position);
+            animator.SetBool("Walk", true);
+        }
+        else 
+        {
+            animator.SetBool("Walk", false);
         }
 
         if(Vector3.Distance(transform.position, target.position) < damageDistance && !attacking) 
         {
             StartCoroutine(AttackCoroutine());
         }
-
-        if(health <= 0) 
-        {
-            Destroy(gameObject);
-        }
     }
 
     IEnumerator AttackCoroutine() {
         attacking = true;
         target.GetComponent<PlayerHealth>().Hurt(damage);
+        animator.SetTrigger("Swing");
         yield return new WaitForSeconds(1.0f);
         attacking = false;
     }
